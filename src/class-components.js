eacef35d93ef9e58186d471f0bda2e6c1ae8ce6c
Delicious_Apple <110074066+Appeleus@users.noreply.github.com>
2023-08-11 15:40:25 +0700

    Added new components
    How do I break link when return or did I misunderstood something I dunno but this is very cool oh ma oh ma god

---
+++ src/class-components.js
@@ -0,0 +1,32 @@
+import React, {Component} from 'react'
+
+export default class Calendar extends Component {
+    getDate() {
+        const dayNames = ['Sunday','Monday','Tuesday',
+                        'Wednesday','Thursday','Friday',
+                        'Saturday']
+        const monthNames = ['January','February','March',
+                            'April','May','June','July',
+                            'August','September','October',
+                            'November','December']
+        const date = new Date()
+        const weekDay = dayNames[date.getDay()]
+        const day = date.getDate()
+        const month = monthNames[date.getMonth()]
+        const year = date.getFullYear() + 543
+        const time = date.getTime()
+        const todayOverallTime = ((time + 25200000) / 1000) % 86400
+        const todaySecond = Math.floor(todayOverallTime % 60)
+        const todayMinute = Math.floor((todayOverallTime / 60) % 60)
+        const todayHour = Math.floor(((todayOverallTime / 60) / 24) % 24)
+        return `Date ${weekDay} on ${day} ${month} ${year} 
+        \n | 
+        Current Time: 
+        ${todayHour}:${todayMinute}:${todaySecond}
+        UTC+7 `
+    }
+
+    render() {
+        return <div>{this.getDate()}</div>
+    }
+}
