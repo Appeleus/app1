7534eccf882a31dd465332446a8f7f0c84d3b4d5
Delicious_Apple <110074066+Appeleus@users.noreply.github.com>
2023-08-18 15:49:11 +0700

    Event Driver added
    JavaScript event Handler question mark here.

--- src/App.js
+++ src/App.js
@@ -3,16 +3,12 @@ import { Header, Content, Footer } from './func-components'
 import Calendar from './class-components'
 import Banner from './banner'
 import {Calculator2} from './calculator'
+import {EventData1} from './event-data'
 
 function App() {
   return (
   <>
-    <Header/>
-    <p><center><Calendar/></center></p>
-    <Content/>
-    <Footer/>
-    <Banner/>
-    <Calculator2/>
+    <EventData1/>
   </>
   )
 }
